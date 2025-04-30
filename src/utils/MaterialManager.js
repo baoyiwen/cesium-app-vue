0902fde053ef0a80beecda5893508a1aacd19016
baoyiwen <511530203@qq.com>
2025-04-30 11:27:49 +0800

    update bugs

--- src/utils/MaterialManager.js
+++ src/utils/MaterialManager.js
@@ -2,7 +2,7 @@
  * @Author: baoyiwen 511530203@qq.com
  * @Date: 2025-04-28 16:33:26
  * @LastEditors: baoyiwen 511530203@qq.com
- * @LastEditTime: 2025-04-30 11:16:49
+ * @LastEditTime: 2025-04-30 11:26:36
  * @FilePath: \cesium-app-vue\src\utils\MaterialManager.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
